863af513c7dfb2ab41901b3805770865b509fce1
Harris Hancock <vortrab@gmail.com>
2015-03-31 18:40:43 -0700

    setInterrupt is not enough

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #include <future>
 #include <iostream>
 
-// Runs an infinite loop and terminates abnormally.
+// Runs an infinite loop and tries to stop the interpreter.
 int main (int argc, char** argv) {
 	QCoreApplication qapp(argc, argv);
 
@@ -22,18 +22,17 @@ int main (int argc, char** argv) {
 	auto mainModule = PythonQt::self()->getMainModule();
 
 	auto futureResult = std::async(std::launch::async, [&] {
-		return mainModule.evalScript("while 1: pass"/*print(\"Hodor\")"*/, Py_file_input);
+		return mainModule.evalScript(
+"while 1:\n"
+"\ttry:\n"
+"\t\twhile 1:\n"
+"\t\t\tpass\n"
+"\texcept:\n"
+"\t\tpass", Py_file_input);
 	});
 
 	if (std::future_status::timeout == futureResult.wait_for(std::chrono::seconds(1))) {
-		// We want to do this instead of terminate: mainModule.abort();
-		// (or .cancel() or .stop())
-
-		std::terminate();
-
-		// this naive attempt predictably crashes:
-		// qDebug() << mainModule.evalScript("import sys; sys.exit()", Py_file_input);
-
+		PythonQt::self()->setInterrupt();
 	}
 
 	qDebug() << futureResult.get();
