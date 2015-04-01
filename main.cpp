2aadeecf1f54ea16f95fe47619757ad94792318c
Harris Hancock <vortrab@gmail.com>
2015-03-31 17:09:04 -0700

    Create infinite loop

--- main.cpp
+++ main.cpp
@@ -3,17 +3,23 @@
 #include <QCoreApplication>
 #include <QDebug>
 
+#include <iostream>
+
 int main (int argc, char** argv) {
 	QCoreApplication qapp(argc, argv);
 
 	// init PythonQt and Python
 	PythonQt::init();
 
+	QObject::connect(PythonQt::self(), &PythonQt::pythonStdOut, [] (QString x) { std::cout << x.toStdString(); });
+	QObject::connect(PythonQt::self(), &PythonQt::pythonStdErr, [] (QString x) { std::cerr << x.toStdString(); });
+	QObject::connect(PythonQt::self(), &PythonQt::pythonHelpRequest, [] (QByteArray x) { std::cout << "** Help Request:" << x.toStdString() << '\n'; });
+
 	// get the __main__ python module
 	auto mainModule = PythonQt::self()->getMainModule();
 
 	// evaluate a simple python script and receive the result a qvariant:
-	auto result = mainModule.evalScript("19*2+4", Py_eval_input);
+	auto result = mainModule.evalScript("while 1: print(\"Hodor\")", Py_file_input);
 
 	qDebug() << result;
 
